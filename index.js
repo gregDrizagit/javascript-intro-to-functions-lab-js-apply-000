17d460c31dfb003d13bd120d43fd82de6b738b84
Greg Driza <gdriza@uarts.edu>
2017-09-01 16:52:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,21 @@ function logShout(string)
   console.log(string);
 }
 
-function logWhisper()
+function logWhisper(string)
 {
-
+  console.log(string)
 }
-function sayHiToGrandma()
+function sayHiToGrandma(string)
 {
+  if(string === lowercase)
+  {
+    return "I can't hear you!"; 
+  }else if (string === uppercase)
+  {
 
+    return "YES INDEED!"; 
+    
+  }else if (string === "I love you, Grandma.")
 
+    return "I love you, too"; 
 }
